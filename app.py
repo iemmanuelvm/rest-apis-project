dd1fe2c20c11ea1538bb08b353fa3ed8a2f9627d
iemmanuelvm <iemmanuelvm@gmail.com>
2023-07-14 08:12:22 -0600

    Modify app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from db import db
 from blocklist import BLOCKLIST
+import os
+
+from dotenv import load_dotenv
 
 from resources.user import blp as UserBlueprint
 from resources.item import blp as ItemBlueprint
@@ -13,6 +16,7 @@ from resources.tag import blp as TagBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
@@ -21,7 +25,7 @@ def create_app(db_url=None):
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
